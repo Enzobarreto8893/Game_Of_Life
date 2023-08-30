5f4263c2883abd744169691b40a2a533df78cf22
Enzobarreto8893 <enzo.barreto@correo.ucu.edu.uy>
2023-08-30 20:57:48 -0300

    cambio b

---
+++ Game_Of_Life-7f7dabb00aaa7f36b11123cddbd91dd0d11a86d8/src/Program/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading;
+
+namespace PII_Game_Of_Life
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool[,] gameBoard = Tablero.Generate();
+            while(true)
+            {
+                Tablero.printBoard(gameBoard);
+                gameBoard = Tablero.next_Generation(gameBoard);
+                Thread.Sleep(300);
+            }
+        }
+    }
+}
